451ea9919ffdeb2b1df456cd3b26c2c31159a5ab
Aucker <aucker2020@gmail.com>
2022-05-10 19:36:59 +0800

    use generic

--- lists/src/second.rs
+++ lists/src/second.rs
@@ -8,26 +8,26 @@
 
 // use std::mem;
 
-pub struct List {
-    head: Link,
+pub struct List<T> {
+    head: Link<T>,
 }
 
-type Link = Option<Box<Node>>;
+type Link<T> = Option<Box<Node<T>>>;
 // enum Link {
 //     Empty,
 //     More(Box<Node>),
 // }
-struct Node {
-    elem: i32,
-    next: Link,
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
 }
 
-impl List {
+impl<T> List<T> {
     pub fn new() -> Self {
         List { head: None }
     }
 
-    pub fn push(&mut self, elem: i32) {
+    pub fn push(&mut self, elem: T) {
         // TODO
         let new_node = Box::new(Node {
             elem: elem,
@@ -39,7 +39,7 @@ impl List {
         self.head = Some(new_node);
     }
 
-    pub fn pop(&mut self) -> Option<i32> {
+    pub fn pop(&mut self) -> Option<T> {
         // let result;
         // match mem::replace(&mut self.head, None) {
         // match self.head.take() {
@@ -60,7 +60,7 @@ impl List {
     }
 }
 
-impl Drop for List {
+impl<T> Drop for List<T> {
     fn drop(&mut self) {
         // let mut cur_link = mem::replace(&mut self.head, None);
         let mut cur_link = self.head.take();
